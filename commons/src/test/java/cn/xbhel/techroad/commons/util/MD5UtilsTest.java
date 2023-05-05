d6e390537634be30a1fd8ca7c7e68d3f4b109403
xbhel <xbhel@outlook.com>
2023-05-05 22:01:16 +0800

    feat#commons_添加 MD5 工具类

--- commons/src/test/java/cn/xbhel/techroad/commons/util/MD5UtilsTest.java
+++ commons/src/test/java/cn/xbhel/techroad/commons/util/MD5UtilsTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MD5UtilsTest {
 
     @Test
-    void givenParagraph_whenEncode_thenVerifyCheckSum() {
+    void givenParagraph_thenVerifyCheckSum() {
         var checksum = "35454b055cc325ea1af2126e27707052";
         var md5Data = MD5Utils.encode("ILoveJava".getBytes(StandardCharsets.UTF_8));
         assertThat(new String(HexUtils.encode(md5Data)))
