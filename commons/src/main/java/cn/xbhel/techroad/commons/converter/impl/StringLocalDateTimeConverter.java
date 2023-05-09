744f95e4698780447e99351a8485cf676ebbc2de
xbhel <xbhel@outlook.com>
2023-05-09 21:18:36 +0800

    feat#commons_创建一个类用于string&localdatetime的相互转换.

---
+++ commons/src/main/java/cn/xbhel/techroad/commons/converter/impl/StringLocalDateTimeConverter.java
@@ -0,0 +1,23 @@
+package cn.xbhel.techroad.commons.converter.impl;
+
+import cn.xbhel.techroad.commons.converter.StringConverter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 字符串和 {@link LocalDateTime} 转换
+ * @author xbhel
+ */
+public class StringLocalDateTimeConverter implements StringConverter<LocalDateTime> {
+
+    @Override
+    public LocalDateTime fromStr(String str) {
+        return null;
+    }
+
+    @Override
+    public String toStr(LocalDateTime object) {
+        return null;
+    }
+
+}
