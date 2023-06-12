fa4e624b37e48b40477c158a81ad350eb45ef33c
xbhel <xbhel@outlook.com>
2023-06-12 23:10:51 +0800

    feat#secure、spring

--- commons/src/test/java/cn/xbhel/techroad/commons/secure/symmetric/AESTest.java
+++ commons/src/test/java/cn/xbhel/techroad/commons/secure/symmetric/AESTest.java
@@ -1,7 +1,11 @@
 package cn.xbhel.techroad.commons.secure.symmetric;
 
 import cn.xbhel.techroad.commons.secure.KeyUtils;
+import cn.xbhel.techroad.commons.util.ByteUtils;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +27,23 @@ class AESTest {
     void createByAlgorithmAndKey() {
         var aes = new AES("AES", KeyUtils.getKey("AES", 256));
         assertThat(aes).isNotNull();
+    }
+
+    @Test
+    void aesEncryption() {
+        var aes = new AES(SymmetricAlgorithm.AES);
+        var encrypt = aes.encrypt(ByteUtils.toBytes("this is a piece of text."));
+        assertThat(encrypt).isNotEmpty();
+    }
+
+    @Test
+    void aesEncryptLargeText() {
+        var aes = new AES(SymmetricAlgorithm.AES);
+        var largeText = Stream.generate(() -> "this is a piece of text.")
+                .limit(5000)
+                .collect(Collectors.joining());
+        var encrypt = aes.encrypt(ByteUtils.toBytes(largeText));
+        assertThat(encrypt).isNotEmpty();
     }
 
 }
